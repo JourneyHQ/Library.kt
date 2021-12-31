bd8a25fd64cbf20686acb9664527ab9f912ba2dd
yuuaHP <yuuahp@icloud.com>
2021-12-31 22:52:02 +0900

    feat: commons

---
+++ src/main/kotlin/LibClassFinder.kt
@@ -0,0 +1,71 @@
+package dev.yuua.journeylib
+
+import java.io.File
+import java.io.IOException
+import java.net.JarURLConnection
+import java.net.URL
+import java.util.*
+
+class LibClassFinder {
+    private val classLoader: ClassLoader = Thread.currentThread().contextClassLoader
+
+    private fun fileNameToClassName(name: String): String {
+        return name.substring(0, name.length - ".class".length)
+    }
+
+    private fun resourceNameToClassName(resourceName: String): String {
+        return fileNameToClassName(resourceName).replace('/', '.')
+    }
+
+    private fun isClassFile(fileName: String): Boolean {
+        return fileName.endsWith(".class")
+    }
+
+    private fun packageNameToResourceName(packageName: String): String {
+        return packageName.replace('.', '/')
+    }
+
+    @Throws(ClassNotFoundException::class, IOException::class)
+    fun findClasses(rootPackageName: String): List<Class<*>> {
+        val resourceName = packageNameToResourceName(rootPackageName)
+        val url = classLoader.getResource(resourceName) ?: return ArrayList()
+        val protocol = url.protocol
+        if ("file" == protocol) {
+            return findClassesWithFile(rootPackageName, File(url.file))
+        } else if ("jar" == protocol) {
+            return findClassesWithJarFile(rootPackageName, url)
+        }
+        throw IllegalArgumentException("Unsupported Class Load Protocol[$protocol]")
+    }
+
+    @Throws(ClassNotFoundException::class)
+    private fun findClassesWithFile(packageName: String, dir: File): List<Class<*>> {
+        val classes: MutableList<Class<*>> = ArrayList()
+        for (path in Objects.requireNonNull(dir.list())) {
+            val entry = File(dir, path)
+            if (entry.isFile && isClassFile(entry.name)) {
+                classes.add(classLoader.loadClass(packageName + "." + fileNameToClassName(entry.name)))
+            } else if (entry.isDirectory) {
+                classes.addAll(findClassesWithFile(packageName + "." + entry.name, entry))
+            }
+        }
+        return classes
+    }
+
+    @Throws(IOException::class, ClassNotFoundException::class)
+    private fun findClassesWithJarFile(rootPackageName: String, jarFileUrl: URL): List<Class<*>> {
+        val classes: MutableList<Class<*>> = ArrayList()
+        val jarUrlConnection = jarFileUrl.openConnection() as JarURLConnection
+        jarUrlConnection.jarFile.use { jarFile ->
+            val jarEnum = jarFile.entries()
+            val packageNameAsResourceName = packageNameToResourceName(rootPackageName)
+            while (jarEnum.hasMoreElements()) {
+                val jarEntry = jarEnum.nextElement()
+                if (jarEntry.name.startsWith(packageNameAsResourceName) && isClassFile(jarEntry.name)) {
+                    classes.add(classLoader.loadClass(resourceNameToClassName(jarEntry.name)))
+                }
+            }
+        }
+        return classes
+    }
+}
---
+++ src/main/kotlin/LibFlow.kt
@@ -0,0 +1,116 @@
+package dev.yuua.journeylib
+
+import dev.yuua.journeylib.LibTermColor.RESET
+
+/**
+ * ### ログを統一された書式で出力します。
+ * @param globalName デフォルトで使用される出力名
+ */
+class LibFlow(private val globalName: String? = null) {
+    enum class Type(val symbol: String, val color: String) {
+        HEADER(":", LibTermColor.BLUE_BRIGHT),
+        INFO("i", LibTermColor.PURPLE_BRIGHT),
+        TASK(">", LibTermColor.YELLOW_BRIGHT),
+        SUCCESS("+", LibTermColor.GREEN_BRIGHT),
+        FAILURE("#", LibTermColor.RED_BRIGHT)
+    }
+
+    /**
+     * ### 指定された情報を出力形式にフォーマットして返します。
+     * @param name 名前
+     * @param type 出力タイプ
+     * @param context 内容
+     */
+    private fun getFormatted(name: String, type: Type, context: String?): String {
+        return "|${type.color + type.symbol + RESET}|[${LibTermColor.BLACK_BRIGHT + name + RESET}] $context"
+    }
+
+    /**
+     * ### HEADERを出力します。
+     * @param name 名前
+     */
+    fun header(name: String): LibFlow {
+        println(getFormatted(name, Type.HEADER, "==="))
+        return this
+    }
+
+    /**
+     * ### INFOを出力します。
+     * @param name 名前
+     * @param context 内容
+     */
+    fun info(name: String? = null, context: String?): LibFlow {
+        println(getFormatted(name ?: globalName ?: "INFO", Type.INFO, context))
+        return this
+    }
+
+    /**
+     * ### INFOを出力します。
+     * 名前を省略してグローバル名を使用します。
+     * @param context 内容
+     */
+    fun info(context: String?): LibFlow {
+        println(getFormatted(globalName ?: "INFO", Type.INFO, context))
+        return this
+    }
+
+    /**
+     * ### TASKを出力します。
+     * @param name 名前
+     * @param context 内容
+     */
+    fun task(name: String? = null, context: String?): LibFlow {
+        println(getFormatted(name ?: globalName ?: "TASK", Type.TASK, context))
+        return this
+    }
+
+    /**
+     * ### TASKを出力します。
+     * 名前を省略してグローバル名を使用します。
+     * @param context 内容
+     */
+    fun task(context: String?): LibFlow {
+        println(getFormatted(globalName ?: "TASK", Type.TASK, context))
+        return this
+    }
+
+    /**
+     * ### SUCCESSを出力します。
+     * @param name 名前
+     * @param context 内容
+     */
+    fun success(name: String? = null, context: String?): LibFlow {
+        println(getFormatted(name ?: globalName ?: "SUCCESS", Type.SUCCESS, context))
+        return this
+    }
+
+    /**
+     * ### SUCCESSを出力します。
+     * 名前を省略してグローバル名を使用します。
+     * @param context 内容
+     */
+    fun success(context: String?): LibFlow {
+        println(getFormatted(globalName ?: "SUCCESS", Type.SUCCESS, context))
+        return this
+    }
+
+    /**
+     * ### FAILUREを出力します。
+     * @param name 名前
+     * @param context 内容
+     */
+    fun failure(name: String? = null, context: String?): LibFlow {
+        println(getFormatted(name ?: globalName ?: "FAILURE", Type.FAILURE, context))
+        return this
+    }
+
+    /**
+     * ### FAILUREを出力します。
+     * 名前を省略してグローバル名を使用します。
+     * @param context 内容
+     */
+    fun failure(context: String?): LibFlow {
+        println(getFormatted(globalName ?: "FAILURE", Type.FAILURE, context))
+        return this
+    }
+}
