900d91c1e5506f1c7b3d49d7f05333fb01d8d7f8
yuuaHP <yuuahp@icloud.com>
2022-01-06 21:03:48 +0900

    feat: LibEmbedColor

---
+++ src/main/kotlin/dev/yuua/journeylib/discord/LibEmbedColor.kt
@@ -0,0 +1,10 @@
+package dev.yuua.journeylib.discord
+
+import java.awt.Color
+
+object LibEmbedColor {
+    val failure = Color(255, 110, 110)
+    val caution = Color(255, 178, 110)
+    val success = Color(132, 199, 118)
+    val normal = Color(206, 132, 196)
+}
