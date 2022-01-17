94b1afd51862b00a13a85b86436d01c0a26cef7b
yuuaHP <yuuahp@icloud.com>
2022-01-17 21:50:07 +0900

    feat: InteractionExtension

---
+++ src/main/kotlin/dev/yuua/journeylib/discord/framework/InteractionExtension.kt
@@ -0,0 +1,16 @@
+package dev.yuua.journeylib.discord.framework
+
+import net.dv8tion.jda.api.entities.Message
+import net.dv8tion.jda.api.entities.MessageEmbed
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
+import net.dv8tion.jda.api.requests.restaction.WebhookMessageAction
+
+class InteractionExtension {
+    fun SlashCommandEvent.followUp(message: String): WebhookMessageAction<Message> {
+        return this.hook.sendMessage(message)
+    }
+
+    fun SlashCommandEvent.followUp(message: MessageEmbed, vararg messages: MessageEmbed): WebhookMessageAction<Message> {
+        return this.hook.sendMessageEmbeds(message, *messages)
+    }
+}
