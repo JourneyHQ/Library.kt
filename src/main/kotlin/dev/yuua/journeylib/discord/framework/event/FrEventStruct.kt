77f22a1402fabc468987e2ae4028458487c5b9d6
yuua <yuuahp@icloud.com>
2022-03-29 16:36:38 +0900

    feat: allow multiple listeners

--- src/main/kotlin/dev/yuua/journeylib/discord/framework/event/FrEventStruct.kt
+++ src/main/kotlin/dev/yuua/journeylib/discord/framework/event/FrEventStruct.kt
@@ -1,8 +1,7 @@
 package dev.yuua.journeylib.discord.framework.event
 
-import dev.minn.jda.ktx.CoroutineEventListener
 import net.dv8tion.jda.api.JDA
 
 interface FrEventStruct {
-    fun listener(jda: JDA): CoroutineEventListener
+    fun listener(jda: JDA): Any
 }
